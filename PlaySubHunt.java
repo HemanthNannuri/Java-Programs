172d979bbcd7e58689c65bad10a342b0f4ac5a70
Hemanth kumar N <nannurihk007@gmail.com>
2023-11-05 23:01:51 -0600

    GUI

---
+++ PlaySubHunt.java
@@ -0,0 +1,15 @@
+/* PlaySubHunt class
+*  Tu
+*/
+
+import javax.swing.JFrame;
+
+public class PlaySubHunt 
+{
+ public static void main( String [] args )
+ {
+  SubHunt sub = new SubHunt( SubHunt.DEFAULT_GAME_SIZE );
+  SubHuntViewController subGui = new SubHuntViewController( sub );
+  subGui.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+ }
+}
